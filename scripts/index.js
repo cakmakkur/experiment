a375cbb145f8c40e07637b79533000a3f90f5872
cakmakkur <146092286+cakmakkur@users.noreply.github.com>
2023-11-09 13:35:13 +0100

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,85 @@
+// this function returns the nextLocationId
+let nextLocationId = '';
+
+function pickLocation () {
+  const randomNumber = Math.random();
+  if (randomNumber >= 0 && randomNumber <= 1/16 ) {
+    nextLocationId = '1'
+  } else if (randomNumber > 1/16 && randomNumber <= 2 / 16 ) {
+    nextLocationId = '2'
+  } else if (randomNumber > 2/16 && randomNumber <= 3 / 16 ) {
+    nextLocationId = '3'
+  } else if (randomNumber > 3/16 && randomNumber <= 4 / 16 ) {
+    nextLocationId = '4'
+  } else if (randomNumber > 4/16 && randomNumber <= 5 / 16)  {
+    nextLocationId = '5'
+  } else if (randomNumber > 5/16 && randomNumber <= 6 / 16)  {
+    nextLocationId = '6'
+  } else if (randomNumber > 6/16 && randomNumber <= 7 / 16 ) {
+    nextLocationId = '7'
+  } else if (randomNumber > 7/16 && randomNumber <= 8 / 16 ) {
+    nextLocationId = '8'
+  } else if (randomNumber > 8/16 && randomNumber <= 9 / 16 ) {
+    nextLocationId = '9'
+  } else if (randomNumber > 9/16 && randomNumber <= 10 / 16 ) {
+    nextLocationId = '10'
+  } else if (randomNumber > 10/16 && randomNumber <= 11 / 16 ) {
+    nextLocationId = '11'
+  } else if (randomNumber > 11/16 && randomNumber <= 12 / 16 ) {
+    nextLocationId = '12'
+  } else if (randomNumber > 12/16 && randomNumber <= 13 / 16 ) {
+    nextLocationId = '13'
+  } else if (randomNumber > 13/16 && randomNumber <= 14 / 16 ) {
+    nextLocationId = '14'
+  } else if (randomNumber > 14/16 && randomNumber <= 15 / 16 ) {
+    nextLocationId = '15'
+  } else if (randomNumber > 15/16 && randomNumber <= 1 ) {
+    nextLocationId = '16'
+  }
+  return nextLocationId;
+  }
+
+// starting function
+let matchingButton = '';
+
+function startGame () {
+
+  let location = pickLocation();
+
+  document.querySelectorAll('.buttonGeneral').forEach((button) => {
+    button.style.display = "none";
+    const buttonId = button.dataset.buttonId;
+
+    if (buttonId === location) {
+      matchingButton = button;
+    }
+  });
+  matchingButton.style.display = "inline-block"
+  matchingButton.removeEventListener('click', makeClickable);
+  matchingButton.addEventListener('click', makeClickable)
+}
+
+function makeClickable () {
+  correctClicks += 1;
+  console.log(correctClicks);
+  matchingButton.removeEventListener('click', makeClickable)
+}
+
+//on/off button
+let correctClicks = 0;
+
+let intervalId = null;
+let isOn = false;
+document.querySelector('.js-start-button').addEventListener('click', () => {
+
+    if (!isOn) {
+      document.querySelector('.results').innerHTML = ``;
+      intervalId = setInterval(startGame, 1000);
+      isOn = true;
+    } else {
+      clearInterval(intervalId);
+      document.querySelector('.results').innerHTML = `You clicked ${correctClicks} times on time`;
+      correctClicks = 0;
+      isOn = false;
+    }
+  })
